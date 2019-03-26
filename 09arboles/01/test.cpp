d989a59f2e58afe138fed7ddab6b37f7be97d90f
David Davó <david@ddavo.me>
2019-03-26 17:52:41 +0100

    Test unitarios de C :3

---
+++ 09arboles/01/test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+TEST(TestingTest, Suma) {
+	ASSERT_EQ(4, 2+2);
+	ASSERT_EQ(5, 2+2);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
