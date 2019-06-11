7ee53fe234078d9ee73a220fed06e277ed1ad705
David Davó <david@ddavo.me>
2019-06-11 16:51:56 +0200

    .

--- 08lineales/01/ej01.cpp
+++ 08lineales/01/ej01.cpp
@@ -4,9 +4,15 @@
 void ej01(ostream s, int n) {
 	Pila pila = Pila<int>();
 
-	while (n / 10 != 0) {
+	while (n > 0) {
+        pila.apila(n%10);
 
-
-		n = n%10;
+		n = n/10;
 	}
+
+    int suma = 0;
+    while (!pila.esVacia()) {
+        suma += pila.cima();
+        s << pila.cima << " + ";
+    }
 }
