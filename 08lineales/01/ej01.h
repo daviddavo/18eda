82bf09ecaa42889246b3aa7c71a7b76b958c65ba
David Davó <david@ddavo.me>
2019-06-11 17:38:36 +0200

    08/01

--- 08lineales/01/ej01.h
+++ 08lineales/01/ej01.h
@@ -1,10 +1,32 @@
 #ifndef __EJ01_h
 #define __EJ01_h
 
+#include <iostream>
+
 /** Implementa, con ayuda de una pila, un procedimiento no recursivo que reciba como parámetro un número entero
 	n >= 0 y escriba por pantalla sus dígitos en orden lexicográfico y su suma.
 	Por ejemplo, ante n = 64323 escribirá */
+void ej01(std::ostream & s, int n) {
+    Pila<int> pila = Pila<int>();
 
-void ej01(ostream s, int n);
+    while (n > 0) {
+        pila.apila(n%10);
+
+        n = n/10;
+    }
+
+    int suma = 0;
+    while (!pila.esVacia()) {
+        suma += pila.cima();
+        s << pila.cima();
+
+        pila.desapila();
+        
+        if (!pila.esVacia())
+            s << " + ";
+    }
+
+    s << " = " << suma;
+}
 
 #endif
--- 08lineales/test.cpp
+++ 08lineales/test.cpp
@@ -1,5 +1,45 @@
 #include <sstream>
+#include <string>
 #include "gtest/gtest.h"
 
+#include "pila.h"
+
+#include "01/ej01.h"
+
+using namespace std;
+
+template <class T, size_t N>
+Pila<T> arrtopila(T (&arr) [N]) {
+    Pila<T> ret = Pila<T>();
+
+    for (int i = 0; i < N; ++i)
+        ret.apila(arr[i]);
+
+    return ret;
+}
+
 class TestLineales : public :: testing::Test {
+protected:
+    Pila<int> p1;
+
+    void SetUp() override {
+        int p1arr[] {6,4,3,2,3};
+        p1 = arrtopila(p1arr);
+    }
+};
+
+class TestLineales01 : public :: TestLineales {};
+
+TEST_F(TestLineales01, suma) {
+    stringstream ss;
+    ej01(ss, 64323);
+    EXPECT_EQ(ss.str(), "6 + 4 + 3 + 2 + 3 = 18");
+    ss.str("");
+    ej01(ss, 12345);
+    EXPECT_EQ(ss.str(), "1 + 2 + 3 + 4 + 5 = 15");
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
