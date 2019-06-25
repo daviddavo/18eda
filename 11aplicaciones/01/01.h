362607608956d4fdda9fa764b66eb2e4356e9e2d
David Davó <david@ddavo.me>
2019-06-25 12:16:54 +0200

    BigVector empezado

---
+++ 11aplicaciones/01/01.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "DiccionarioHash.h"
+
+class BigVector {
+    DiccionarioHash<unsigned long, double> _dicc;
+
+    public:
+    BigVector() : _dicc() {}
+
+    void ponValor(unsigned long i, double val) {
+        if (val == 0) _dicc.borra(i);
+        else _dicc.inserta(i, val);
+    }
+
+    double valorDe(unsigned long i) {
+        if (!_dicc.contiene(i)) return 0;
+        return _dicc.valorPara(i);
+    }
+
+    BigVector & productoEscalar(const BigVector & other) const {
+        for (auto it = _dicc.cbegin(), it != _dicc.cend(); it.next()) {
+            
+        }
+    }
+};
--- 11aplicaciones/test.cpp
+++ 11aplicaciones/test.cpp
@@ -1,5 +1,40 @@
 #include <sstream>
 #include "gtest/gtest.h"
 
-class TestAplicaciones : public :: testing::Test {
+#include "01/01.h"
+
+class TestAplicaciones : public :: testing::Test {};
+
+class TestAplicaciones01 : public :: TestAplicaciones {
+    protected:
+    BigVector _bv0;
+    BigVector _bv1;
+
+    void SetUp() override {
+        // _bv0 always 0
+        for (int i = 0; i < 255; i++)
+            _bv1.ponValor(i, i);
+    }
+};
+
+TEST_F(TestAplicaciones01, BigVector_create) {
+    ASSERT_NO_THROW(BigVector());
+    for (int i = 0; i < 265; ++i)
+        ASSERT_EQ(_bv0.valorDe(i), 0);
+}
+
+TEST_F(TestAplicaciones01, BigVector_ponValorValorDe) {
+    for (int i = 0; i < 255; ++i)
+        ASSERT_EQ(_bv1.valorDe(i), i);
+
+    for (int i = 0; i < 5; i++)
+        _bv1.ponValor(i, 0);
+
+    for (int i = 0; i < 5; ++i)
+        ASSERT_EQ(_bv1.valorDe(i), 0);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
