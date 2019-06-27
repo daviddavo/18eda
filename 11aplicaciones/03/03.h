127573e482cee5e2803bf5aa98744502f73e2e7a
David Davó <david@ddavo.me>
2019-06-27 17:36:08 +0200

    TBC

---
+++ 11aplicaciones/03/03.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "diccionario.h"
+
+/**
+ * Ejercicio 3:
+ * (Extraído del examen extraordinario de febrero 2010)
+ * Se desea definir un TAD Agencia que represente a una agencia de viajes y permita
+ * realizar gestiones básicas. El TAD debe ofrecer las siguientes operaciones:
+ */
+
+class AgenciaViajes {
+
+    public:
+        AgenciaViajes() {
+
+        }
+};
