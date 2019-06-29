ca60df39af60547266db360d8ae646893af84d1f
David Davó <david@ddavo.me>
2019-06-29 21:05:43 +0200

    wip

---
+++ 12examenes/2019Junio/ej3/test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+#include "SistemaVentas.h"
+
+class TestVentas : public ::testing::Test {
+    
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
