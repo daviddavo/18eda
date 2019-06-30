803bd20987e61d7c4080114cb438282fa7cc9331
David Davó <david@ddavo.me>
2019-06-30 19:59:47 +0200

    Fixed Compilation Error

--- 12examenes/2019Junio/ej3/hash.h
+++ 12examenes/2019Junio/ej3/hash.h
@@ -1,6 +1,8 @@
 #ifndef __HASH_H
 #define __HASH_H
 
+#include <functional>
+
  // AQUI DEBEN DECLARARSE LA FUNCION O FUNCIONES h QUE
  // SEAN NECESARIAS (deben ponerse las cabeceras).
  // LAS FUNCIONES DEBEN IMPLEMENTARSE EN UN ARCHIVO .cpp
